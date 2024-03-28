4ded70639e327be0182fc0415054c4a6f8e65240
Michal Srb <srb@convex.dev>
2024-03-28 14:32:24 +0100

    Vector search

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,10 +5,16 @@ export default defineSchema({
   messages: defineTable({
     author: v.string(),
     body: v.string(),
+    embedding: v.optional(v.array(v.number())),
   })
     .index("author", ["author"])
     .searchIndex("body", {
       searchField: "body",
       filterFields: ["author"],
+    })
+    .vectorIndex("embedding", {
+      vectorField: "embedding",
+      filterFields: ["author", "body"],
+      dimensions: 1536,
     }),
 });
