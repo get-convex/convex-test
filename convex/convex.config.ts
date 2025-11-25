53c55d6465d270f6b1cdac1421d864b8c5e146b5
Ian Macartney <ianhmacartney@gmail.com>
2025-11-25 14:15:28 -0800

    move counter to be nested

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -1,5 +1,5 @@
 import { defineApp } from "convex/server";
-import counter from "../counter/component/convex.config";
+import counter from "./counter/component/convex.config.js";
 
 const app = defineApp();
 app.use(counter);
